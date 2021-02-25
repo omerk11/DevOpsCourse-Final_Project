8a86ff6b6aa27d7ab2f6d6f18e82edac9f004382
omerk11 <77975182+omerk11@users.noreply.github.com>
2021-02-25 11:20:05 +0200

    Create new Project

---
+++ Project2/frontend_testing.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+try:
+    driver = webdriver.Chrome(executable_path="/Users/omerk/Downloads/chromedriver")
+    driver.get("http://127.0.0.1:5001/users/get_user_data/1")
+    name = driver.find_element_by_id('user').text
+    print(name)
+except:
+    print('Error!')
+
+finally:
+    time.sleep(3)
+    driver.quit()
+
---
+++ Project2/web_app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from db_connector import *
+import signal
+import os
+app = Flask(__name__)
+
+
+# supported methods
+@app.route('/users/get_user_data/<user_id>')
+def user(user_id):
+    try:
+        name = get_user_info(user_id)
+        return "<H1 id='user'>" + name + "</H1>", 200
+    except:
+        return "<H1 id='error'>""no such user: " + user_id + "</H1>", 500
+
+@app.route('/stop_server')
+def stop_server():
+    os.kill(os.getpid(), signal.SIGINT)
+    return 'Server stopped', 200
+
+
+app.run(host='127.0.0.1', debug=True, port=5001)
+
