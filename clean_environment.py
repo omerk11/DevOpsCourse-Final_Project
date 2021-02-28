0bc64eedde27dfb8d8420e5cc1e7c44a2450cf65
omerk11 <77975182+omerk11@users.noreply.github.com>
2021-02-28 12:53:57 +0200

    Update clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -1,8 +1,7 @@
 import requests
 
 try:
-    requests.get('http://127.0.0.1:5000/stop_server', timeout=5)
-    requests.get('http://127.0.0.1:5001/stop_server', timeout=5)
+    requests.get('http://0.0.0.0:5000/stop_server', timeout=5)
     print("Backend servers stopped")
 
 except requests.exceptions.ReadTimeout:
