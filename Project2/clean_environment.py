68335646de787b211c5ee046468aa43f448618b9
omerk11 <77975182+omerk11@users.noreply.github.com>
2021-02-28 16:33:46 +0200

    Delete Project2 directory

--- Project2/clean_environment.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-try:
-    requests.get('http://127.0.0.1:5000/stop_server', timeout=5)
-    requests.get('http://127.0.0.1:5001/stop_server', timeout=5)
-    print("Backend servers stopped")
-
-except requests.exceptions.ReadTimeout:
-    print("The request timeout -  504 error")
